b81eef459b5876986d377ba75a9a34532a4b4ebf
Hidde Stokvis <hidde@neverhide.nl>
2017-09-25 16:14:31 +0200

    fix: live link

--- src/constants.tsx
+++ src/constants.tsx
@@ -1,1 +1,1 @@
-export const API_URL: string = 'http://localhost:3000';
+export const API_URL: string = (process.env.NODE_ENV === 'production') ? 'http://api.twelveprojects.nl' : 'http://localhost:3000';
